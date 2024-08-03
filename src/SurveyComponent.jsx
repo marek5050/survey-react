8635fd15df322b69c0822042e0af0fee99b93075
Marek <marek5050@users.noreply.github.com>
2024-08-03 06:13:48 -0400

    Initial commit

---
+++ src/SurveyComponent.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Model } from "survey-core";
+import { Survey } from "survey-react-ui";
+import "survey-core/defaultV2.min.css";
+import "./index.css";
+import { json } from "./json";
+
+function validateCountry(_, { data, errors, complete }) {
+  const countryName = data["country"];
+  if (!countryName) {
+    complete();
+    return;
+  }
+  fetch("https://surveyjs.io/api/CountriesExample?name=" + countryName)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const found = data.length > 0;
+      if (!found) {
+        errors["country"] = "Country is not found";
+      }
+      complete();
+    });
+}
+function SurveyComponent() {
+    const survey = new Model(json);
+    survey.onComplete.add((sender, options) => {
+        console.log(JSON.stringify(sender.data, null, 3));
+    });
+    survey.onServerValidateQuestions.add(validateCountry);
+    return (<Survey model={survey} />);
+}
+
+export default SurveyComponent;
---
+++ src/json.js
@@ -0,0 +1,11 @@
+export const json = {
+  "elements": [
+    {
+      "type": "text",
+      "name": "country",
+      "title": "Enter a country",
+      "isRequired": true
+    }
+  ],
+  "showQuestionNumbers": false
+};
